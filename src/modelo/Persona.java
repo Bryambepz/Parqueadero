dc3133415f2fdc0ef47f8d7c55c53d6f63a15991
Bryambepz <bryamparra2709@gmail.com>
2021-11-21 16:25:34 -0500

    Creacion de las clases del modelo

--- src/modelo/Persona.java
+++ src/modelo/Persona.java
@@ -12,5 +12,59 @@ public class Persona {
     private String id;
     private String nombre;
     private String email;
+    private Vehiculo vehiculo;
+
+    public Persona() {
+    }
+
+    public Persona(String id, String nombre, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public Persona(String id, String nombre, String email, Vehiculo vehiculo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.vehiculo = vehiculo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "id=" + id + ", nombre=" + nombre + ", email=" + email + '}';
+    }
     
 }
---
+++ src/modelo/Vehiculo.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author braya
+ */
+public class Vehiculo {
+    private String placa;
+    private String color;
+    private String modelo;
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo{" + "placa=" + placa + ", color=" + color + ", modelo=" + modelo + '}';
+    }
+    
+}
