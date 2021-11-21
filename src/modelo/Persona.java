5937cdb833c2261b3d938dfa2cc179de2aa1761b
Bryambepz <bryamparra2709@gmail.com>
2021-11-21 16:07:58 -0500

    Inicio de la pratica de parqueadero con swing

---
+++ src/modelo/Persona.java
@@ -0,0 +1,16 @@
+/*
+ * bpClick nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * bpClick nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author braya
+ */
+public class Persona {
+    private String id;
+    private String nombre;
+    private String email;
+    
+}
